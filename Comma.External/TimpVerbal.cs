a7916e4e4a93acd737a41c6b72089e97c9d5e927
Daniel Zlatea <dzlatea@pentalog.com>
2018-03-05 20:51:27 +0200

    TimpVerbal complet

--- Comma.External/TimpVerbal.cs
+++ Comma.External/TimpVerbal.cs
@@ -1,4 +1,6 @@
-﻿namespace Comma.External
+﻿using System.Collections.Generic;
+
+namespace Comma.External
 {
     public class TimpVerbal
     {
@@ -8,11 +10,10 @@
         }
 
         public string Timp { get; set; }
+
         public string EuPronume { get; set; }
         public string Eu { get; set; }
         public string EuComplete => EuPronume + " " + Eu;
-
-
         public string TuPronume { get; set; }
         public string Tu { get; set; }
         public string TuComplete => TuPronume + " " + Tu;
@@ -28,5 +29,16 @@
         public string ElePronume { get; set; }
         public string Ele { get; set; }
         public string EleComplete => ElePronume + " " + Ele;
+    }
+
+    public class TimpVerbalComplet
+    {
+        public List<TimpVerbal> TimpuriRegulate { get; set; }
+        public string Infinitiv { get; set; }
+        public string ImperativSingular { get; set; }
+        public string ImperativPlural { get; set; }
+        public string InfinitivLung { get; set; }
+        public string Participiu { get; set; }
+        public string Gerunziu { get; set; }
     }
 }
